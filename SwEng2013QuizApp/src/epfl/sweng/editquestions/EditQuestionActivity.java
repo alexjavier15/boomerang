f3cc5d95d97a928bffb532cedd8a2f63986fe55d
Javier Rivas <javieralejandro.rivas@epfl.ch>
2013-09-24 19:08:05 +0200

    added editquestion pck/class

---
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -0,0 +1,25 @@
+package epfl.sweng.editquestions;
+
+import epfl.sweng.R;
+import epfl.sweng.R.layout;
+import epfl.sweng.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EditQuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_question);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.edit_question, menu);
+		return true;
+	}
+
+}
