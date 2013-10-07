2ccb777e6b0dde680d1ffb6bbf8153bb570ca6d0
Alban Marguet <alban.marguet@epfl.ch>
2013-10-07 21:38:15 +0200

    enable testing from command line

---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -0,0 +1,15 @@
+package epfl.sweng.test;
+import android.test.ActivityInstrumentationTestCase2;
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+
+public class ShowQuestionsActivityTest extends
+		ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+
+	public ShowQuestionsActivityTest() {
+		super(ShowQuestionsActivity.class);
+	}
+
+	public void testHello() {
+		fail("Not yet implemented");
+	}
+}
