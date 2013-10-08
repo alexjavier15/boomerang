881193d80b898f61fa1e16f0a03add3ce75abab8
Noortje Verstegen <noortje.verstegen@epfl.ch>
2013-10-08 20:46:43 +0200

    Just some JavaDoc in TestingTransaction interface that was missing

--- SwEng2013QuizApp/src/epfl/sweng/testing/TestingTransaction.java
+++ SwEng2013QuizApp/src/epfl/sweng/testing/TestingTransaction.java
@@ -1,5 +1,12 @@
 package epfl.sweng.testing;
 
+/**
+ * A TestingTransaction encapsulates a complete action that happens in a
+ * program. It is initiated, for example through a click on a button. The action
+ * then causes some processing to happen, and ends when the state of the
+ * application has been updated. Once the transaction completed, the resulting
+ * application state can be verified for consistency and correctness.
+ */
 public interface TestingTransaction {
 	
 	/// Contains the code to initiate the transaction. For example, this might
