156c175e6f0526067ad12b159401edff55381555
JavierRivas <javieralejandro.rivas@epfl.ch>
2013-10-15 17:31:43 +0200

    first showquestion test solved. added mockserver to swengfactory. added reponses for mock server and solved some "string issues" in asserts ( Life instead og life , etc) :)

--- SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -1,11 +1,15 @@
 package epfl.sweng.test;
 
+import org.apache.http.HttpStatus;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 
 import com.jayway.android.robotium.solo.Solo;
 
 import epfl.sweng.entry.MainActivity;
+import epfl.sweng.minimalmock.MockHttpClient;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.showquestions.ShowQuestionsActivity;
 import epfl.sweng.testing.TestCoordinator;
 import epfl.sweng.testing.TestCoordinator.TTChecks;
@@ -17,21 +21,37 @@ public class ShowQuestionsActivityTest extends
 	private Solo solo;
 
 	public ShowQuestionsActivityTest() {
-		
-		
+
 		super(ShowQuestionsActivity.class);
+		// SwengHttpClientFactory.setInstance(new MockHttpClient());
 	}
 
 	@Override
-	protected void setUp() {
+	protected void setUp() throws Exception {
+
+		super.setUp();
+
+		MockHttpClient httpClient = new MockHttpClient();
+		httpClient
+				.pushCannedResponse(
+						"GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
+						HttpStatus.SC_OK,
+						"{\"question\": \"What is the answer to life, the universe, and everything?\","
+								+ " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
+								+ " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
+						"application/json");
+		SwengHttpClientFactory.setInstance(httpClient);
+
 		solo = new Solo(getInstrumentation());
+		;
+
 	}
 
 	public void testShowQuestion() {
 		getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
 		assertTrue(
-				"Question is displayed",
-				solo.searchText("What is the answer to Life, the universe and everything?"));
+				"Question is displayed :" + solo.getText(0).getText(),
+				solo.searchText("What is the answer to life, the universe, and everything?"));
 		assertTrue("Correct answer is displayed", solo.searchText("Forty-two"));
 		assertTrue("Incorrect answer is displayed",
 				solo.searchText("Twenty-seven"));
@@ -41,8 +61,7 @@ public class ShowQuestionsActivityTest extends
 				nextQuestionButton.isEnabled());
 	}
 
-	private void getActivityAndWaitFor(
-			final TestCoordinator.TTChecks expected) {
+	private void getActivityAndWaitFor(final TestCoordinator.TTChecks expected) {
 		TestCoordinator.run(getInstrumentation(), new TestingTransaction() {
 			@Override
 			public void initiate() {
