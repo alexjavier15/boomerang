8c42f02eeeab981313b93bfaf1e76c81acbffe03
Noortch <noortje.verstegen@epfl.ch>
2013-10-16 11:22:22 +0200

    Don't remember what I did...

--- SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionsActivityTest.java
@@ -1,14 +1,19 @@
 package epfl.sweng.test;
 
+import android.widget.EditText;
 import epfl.sweng.editquestions.EditQuestionActivity;
+import epfl.sweng.questions.QuizQuestion;
 
 public class EditQuestionsActivityTest extends TestTemplate<EditQuestionActivity> {
+	
+	QuizQuestion question = createQuestionUniverse();
 
 	public EditQuestionsActivityTest() {
 		super(EditQuestionActivity.class);
 	}
 	
-	
-	
-
+	public void testSubmitQuestion() {
+		getActivity();
+		solo.enterText((EditText) getActivity().findViewById(R.id.edit_questionText), question.getQuestion());
+	}
 }
--- SwEng2013QuizAppTest/src/epfl/sweng/test/HttpTest.java
+++
@@ -1,7 +0,0 @@
-package epfl.sweng.test;
-
-import junit.framework.TestCase;
-
-public class HttpTest extends TestCase {
-
-}
