e52d1cf62a271c8f61e83c8e2155b8755ebbcd6b
JavierRivas <javieralejandro.rivas@epfl.ch>
2013-10-17 06:20:24 +0200

    added little test for editquestion

--- SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionsActivityTest.java
@@ -1,26 +1,64 @@
 package epfl.sweng.test;
 
+import android.test.UiThreadTest;
 import android.widget.Button;
+import android.widget.EditText;
 import epfl.sweng.editquestions.EditQuestionActivity;
 
 public class EditQuestionsActivityTest extends TestTemplate<EditQuestionActivity> {
 
 	Button submit;
-	
+	String question = "Pourquoi suis je si con?";
+	String answer1 = "à cause de la cigarrette";
+	String answer2 = "de naissance";
+	String answer3 = "pourquoi pas?";
+
 	public EditQuestionsActivityTest() {
 		super(EditQuestionActivity.class);
+
 	}
-	
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		// myActivity = getActivity();
+	}
+
 	public void testButtonsMustBeDisplayed() {
-		getActivity();
-		assertTrue("EditText for the question", solo.searchText("question"));
-		assertTrue("EditText for an answer", solo.searchText("answer"));
+
+		assertTrue("EditText for the question", solo.searchText("Type in the question’s text body"));
+		assertTrue("EditText for an answer", solo.searchText("Type in the answer"));
 		assertTrue("Button to remove answer", solo.searchButton("-"));
 		assertTrue("Button to add an answer", solo.searchButton("+"));
-		assertTrue("EditText for the tags", solo.searchText("tags"));
+		assertTrue("EditText for the tags", solo.searchText("Type in the question’s tag"));
 		assertTrue("Button to submit", solo.searchButton("Submit"));
-		
+
 		submit = solo.getButton("Submit");
 		assertFalse("Submit button should be disabled", submit.isEnabled());
+
 	}
+
+	@UiThreadTest
+	public void testButtonStayDissabled() {
+
+		EditText questionT = solo.getEditText("question");
+		questionT.setText("test question");
+		assertFalse("question body must be displayed", questionT.getText().toString().equals(question));
+		questionT.setText(question);
+		assertTrue("question body must be displayed", questionT.getText().toString().equals(question));
+
+		submit = solo.getButton("Submit");
+		assertFalse("Submit button should be disabled", submit.isEnabled());
+		EditText answerT = solo.getEditText("answer");
+		answerT.setText(answer1);
+		assertTrue("answer must be displayed", answerT.getText().toString().equals(answer1));
+		submit = solo.getButton("Submit");
+		assertFalse("Submit button should be disabled", submit.isEnabled());
+		EditText tagsT = solo.getEditText("tag");
+		tagsT.setText("test");
+		assertFalse("answer must be displayed", tagsT.getText().toString().equals(" test"));
+		assertTrue("answer must be displayed", tagsT.getText().toString().equals("test"));
+	}
+
 }
--- SwEng2013QuizAppTest/src/epfl/sweng/test/TestTemplate.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/TestTemplate.java
@@ -14,35 +14,35 @@ import epfl.sweng.questions.QuizQuestion;
 import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.testing.TestCoordinator;
 import epfl.sweng.testing.TestingTransaction;
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
-
 
 @SuppressWarnings("rawtypes")
 public class TestTemplate<T> extends ActivityInstrumentationTestCase2 {
-	
+
 	private MockHttpClient httpClient;
-    protected Solo solo;
+	protected Solo solo;
+	Activity myActivity;
 
 	@SuppressWarnings("unchecked")
 	public TestTemplate(Class activityClass) {
 		super(activityClass);
 	}
-	
+
 	@Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        httpClient = new MockHttpClient();
-        httpClient.pushCannedResponse(
-				"GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
-				HttpStatus.SC_OK,
-				"{\"question\": \"What is the answer to life, the universe, and everything?\","
-						+ " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
-						+ " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
-				"application/json");
-        SwengHttpClientFactory.setInstance(httpClient);
-        solo = new Solo(getInstrumentation());
-    }
-	
+	protected void setUp() throws Exception {
+		super.setUp();
+		httpClient = new MockHttpClient();
+		httpClient.pushCannedResponse("GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
+			HttpStatus.SC_OK,
+			"{\"question\": \"What is the answer to life, the universe, and everything?\","
+				+ " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
+				+ " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
+			"application/json");
+		SwengHttpClientFactory.setInstance(httpClient);
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
 	protected void getActivityAndWaitFor(final TestCoordinator.TTChecks expected) {
 		TestCoordinator.run(getInstrumentation(), new TestingTransaction() {
 			@Override
@@ -52,9 +52,8 @@ public class TestTemplate<T> extends ActivityInstrumentationTestCase2 {
 
 			@Override
 			public void verify(TestCoordinator.TTChecks notification) {
-				assertEquals(String.format(
-						"Expected notification %s, but received %s", expected,
-						notification), expected, notification);
+				assertEquals(String.format("Expected notification %s, but received %s", expected,
+					notification), expected, notification);
 			}
 
 			@Override
