bc7d6404a61552db32279eee64c0d8570ad46be5
Alban Marguet <alban.marguet@epfl.ch>
2013-10-18 19:59:54 +0200

    1 more file

--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -20,6 +20,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import epfl.sweng.R;
 import epfl.sweng.questions.QuizQuestion;
 import epfl.sweng.servercomm.HttpComms;
@@ -31,209 +32,237 @@ import epfl.sweng.tools.Debug;
 import epfl.sweng.tools.JSONParser;
 
 /**
-*
-* @author AlbanMarguet & LorenzoLeon
-*
-*/
+ * 
+ * @author AlbanMarguet & LorenzoLeon
+ * 
+ */
 
-public class ShowQuestionsActivity extends Activity implements HttpcommunicationsAdapter {
+public class ShowQuestionsActivity extends Activity implements
+		HttpcommunicationsAdapter {
 
-    private ArrayAdapter<String> adapter;
-    private ListView answerChoices;
-    private OnItemClickListener answerListener = null;
-    private QuizQuestion currrentQuestion;
-    private int lastChoice = -1;
-    private TextView tags;
-    private TextView text;
+	public static final String ERROR_MESSAGE = "There was an error retrieving the question";
+	private ArrayAdapter<String> adapter;
+	private ListView answerChoices;
+	private OnItemClickListener answerListener = null;
+	private QuizQuestion currrentQuestion;
+	private int lastChoice = -1;
+	private TextView tags;
+	private TextView text;
 
-    /**
-* Launches fetchNewQuestion() when clicking on the button labeled "Next Question"
-*
-* @param view
-*/
-    public void askNextQuestion(View view) {
-        answerChoices.setOnItemClickListener(answerListener);
-        ((Button) findViewById(R.id.next_question)).setEnabled(false);
-        fetchNewQuestion();
-    }
+	/**
+	 * Launches fetchNewQuestion() when clicking on the button labeled
+	 * "Next Question"
+	 * 
+	 * @param view
+	 */
+	public void askNextQuestion(View view) {
+		answerChoices.setOnItemClickListener(answerListener);
+		((Button) findViewById(R.id.next_question)).setEnabled(false);
+		fetchNewQuestion();
+	}
 
-    /**
-* Get the tags of the question to display them on the screen
-*
-* @param list
-* : set of Strings
-* @return the tags
-*/
-    private String displayTags(List<String> list) {
-        if (list.size() > 0) {
-            System.out.println("Va afficher les tags");
-            String tagsInString = "";
-            int counter = 0;
+	/**
+	 * Get the tags of the question to display them on the screen
+	 * 
+	 * @param list
+	 *            : set of Strings
+	 * @return the tags
+	 */
+	private String displayTags(List<String> list) {
+		if (list.size() > 0) {
+			System.out.println("Va afficher les tags");
+			String tagsInString = "";
+			int counter = 0;
 
-            for (String s : list) {
-                counter++;
-                if (counter == list.size()) {
-                    tagsInString += s;
-                } else {
-                    tagsInString += s + ", ";
-                }
-            }
+			for (String s : list) {
+				counter++;
+				if (counter == list.size()) {
+					tagsInString += s;
+				} else {
+					tagsInString += s + ", ";
+				}
+			}
 
-            return tagsInString;
-        } else {
-            return "No tags for this question";
-        }
-    }
+			return tagsInString;
+		} else {
+			return "No tags for this question";
+		}
+	}
 
-    /**
-* Obtains a random question thru an AsyncTask but blocks the thread until the response is received.
-*
-* @return HttpResponse
-*/
+	/**
+	 * Obtains a random question thru an AsyncTask but blocks the thread until
+	 * the response is received.
+	 * 
+	 * @return HttpResponse
+	 */
 
-    public HttpResponse fetchFirstQuestion() {
-        HttpResponse response = null;
+	public HttpResponse fetchFirstQuestion() {
+		HttpResponse response = null;
 
-        Debug.out("Start fetching");
-        try {
-            response = new HttpCommsBackgroundTask(this, false).execute().get();
-        } catch (InterruptedException e) {
-            Log.e(getLocalClassName(), "AsyncTask thread exception");
-        } catch (ExecutionException e) {
-            Log.e(getLocalClassName(), "AsyncTask thread exception");
+		Debug.out("Start fetching");
+		try {
+			response = new HttpCommsBackgroundTask(this, false).execute().get();
+		} catch (InterruptedException e) {
+			Log.e(getLocalClassName(), "AsyncTask thread exception");
+		} catch (ExecutionException e) {
+			Log.e(getLocalClassName(), "AsyncTask thread exception");
+		} finally {
+			if (response == null) {
+				Toast.makeText(this, ERROR_MESSAGE, Toast.LENGTH_LONG).show();
+			}
+		}
 
-        }
+		return response;
+	}
 
-        return response;
-    }
+	/**
+	 * Launches the HTTPGET operation to display a new random question
+	 */
+	public void fetchNewQuestion() {
 
-    /**
-* Launches the HTTPGET operation to display a new random question
-*/
-    public void fetchNewQuestion() {
+		new HttpCommsBackgroundTask(this, true).execute();
 
-        new HttpCommsBackgroundTask(this, true).execute();
+	}
 
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_questions);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_questions);
+		((Button) findViewById(R.id.next_question)).setEnabled(false);
+		answerChoices = (ListView) findViewById(R.id.answer_choices);
 
-        ((Button) findViewById(R.id.next_question)).setEnabled(false);
-        answerChoices = (ListView) findViewById(R.id.answer_choices);
+		tags = (TextView) findViewById(R.id.show_tags);
 
-        tags = (TextView) findViewById(R.id.show_tags);
+		text = (TextView) findViewById(R.id.show_question);
+		Debug.out(text);
 
-        text = (TextView) findViewById(R.id.show_question);
-        Debug.out(text);
+		answerListener = new OnItemClickListener() {
 
-        answerListener = new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> listAdapter, View view,
+					int selectedAnswer, long arg3) {
 
-            @Override
-            public void onItemClick(AdapterView<?> listAdapter, View view, int selectedAnswer, long arg3) {
+				ListView list = (ListView) listAdapter;
+				TextView textListener = (TextView) list
+						.getChildAt(selectedAnswer);
 
-                ListView list = (ListView) listAdapter;
-                TextView textListener = (TextView) list.getChildAt(selectedAnswer);
+				if (lastChoice != -1) {
+					TextView lastChild = (TextView) list.getChildAt(lastChoice);
+					if (lastChild != null) {
+						String lastAnswer = lastChild.getText().toString();
+						lastAnswer = lastAnswer.substring(0,
+								lastAnswer.length() - 1);
+						lastChild.setText(lastAnswer);
+					}
+				}
 
-                if (lastChoice != -1) {
-                    TextView lastChild = (TextView) list.getChildAt(lastChoice);
-                    if (lastChild != null) {
-                        String lastAnswer = lastChild.getText().toString();
-                        lastAnswer = lastAnswer.substring(0, lastAnswer.length() - 1);
-                        lastChild.setText(lastAnswer);
-                    }
-                }
+				String question = getResources().getString(
+						R.string.heavy_ballot_x);
 
-                String question = getResources().getString(R.string.heavy_ballot_x);
+				if (currrentQuestion.checkAnswer(selectedAnswer)) {
+					question = getResources().getString(
+							R.string.heavy_check_mark);
+					((Button) findViewById(R.id.next_question))
+							.setEnabled(true);
+					list.setOnItemClickListener(null);
+					// answerChoices.setOnClickListener(null);
 
-                if (currrentQuestion.checkAnswer(selectedAnswer)) {
-                    question = getResources().getString(R.string.heavy_check_mark);
-                    ((Button) findViewById(R.id.next_question)).setEnabled(true);
-                    list.setOnItemClickListener(null);
-                    // answerChoices.setOnClickListener(null);
+					TestCoordinator.check(TTChecks.ANSWER_SELECTED);
 
-                    TestCoordinator.check(TTChecks.ANSWER_SELECTED);
+				}
 
-                }
+				String newText = textListener.getText().toString() + question;
+				textListener.setText(newText);
+				lastChoice = selectedAnswer;
 
-                String newText = textListener.getText().toString() + question;
-                textListener.setText(newText);
-                lastChoice = selectedAnswer;
+			}
 
-            }
+		};
+		answerChoices.setOnItemClickListener(answerListener);
+		processHttpReponse(fetchFirstQuestion());
+	}
 
-        };
-        answerChoices.setOnItemClickListener(answerListener);
-        processHttpReponse(fetchFirstQuestion());
-    }
+	/**
+	 * Inflate the menu; this adds items to the action bar if it is present.
+	 * 
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.show_questions, menu);
+		return true;
+	}
 
-    /**
-* Inflate the menu; this adds items to the action bar if it is present.
-*
-*/
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.show_questions, menu);
-        return true;
-    }
+	@Override
+	public void processHttpReponse(HttpResponse httpResponse) {
+		if (httpResponse != null) {
+			Debug.out(httpResponse);
+			QuizQuestion quizQuestion = null;
+	
+			try {
+				quizQuestion = JSONParser.parseJsonToQuiz(httpResponse);
+				Debug.out(quizQuestion);
+			} catch (JSONException e) {
+				text.append("/n No question can be obtained !");
+				Log.e(getLocalClassName(), e.getMessage());
+				return;
+			} catch (IOException e) {
+				text.append("/n No question can be obtained !");
+				Log.e(getLocalClassName(), e.getMessage());
+				return;
+			} finally {
+				if (quizQuestion == null) {
+					Toast.makeText(this, ERROR_MESSAGE, Toast.LENGTH_LONG).show();
+				}
+			}
+	
+			if (text == null && quizQuestion != null) {
+				Debug.out("null textview");
+				Toast.makeText(this, ERROR_MESSAGE, Toast.LENGTH_LONG).show();
+			} else {
+				// We've got a satisfying question => treating it
+				currrentQuestion = quizQuestion;
+	
+				text.setText(quizQuestion.getQuestion());
+				tags.setText(displayTags(quizQuestion.getTags()));
+				adapter = new ArrayAdapter<String>(this,
+						android.R.layout.simple_list_item_1,
+						quizQuestion.getAnswers());
+	
+				answerChoices.setAdapter(adapter);
+	
+				adapter.setNotifyOnChange(true);
+	
+			}
+		} else {
+			Toast.makeText(this, ERROR_MESSAGE, Toast.LENGTH_LONG).show();
+		}
+		Debug.out("text : " + text);
+		TestCoordinator.check(TTChecks.QUESTION_SHOWN);
+	}
 
-    @Override
-    public void processHttpReponse(HttpResponse httpResponse) {
-        Debug.out(httpResponse);
-        QuizQuestion quizQuestion = null;
+	@Override
+	public HttpResponse requete() {
+		HttpResponse response = null;
 
-        try {
-            quizQuestion = JSONParser.parseJsonToQuiz(httpResponse);
-            Debug.out(quizQuestion);
-        } catch (JSONException e) {
-            text.append("/n No question can be obtained !");
-            Log.e(getLocalClassName(), e.getMessage());
-            return;
-        } catch (IOException e) {
-            text.append("/n No question can be obtained !");
-            Log.e(getLocalClassName(), e.getMessage());
-            return;
-        }
+		try {
+			response = HttpComms.getInstance(this).getHttpResponse();
+		} catch (NetworkErrorException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return response;
+	}
 
-        if (text == null && quizQuestion != null) {
-            Debug.out("null textview");
-        } else {
-            // We've got a satisfying question => treating it
-            currrentQuestion = quizQuestion;
+	public TextView getText() {
+		return text;
+	}
 
-            text.setText(quizQuestion.getQuestion());
-            tags.setText(displayTags(quizQuestion.getTags()));
-            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quizQuestion.getAnswers());
-
-            answerChoices.setAdapter(adapter);
-
-            adapter.setNotifyOnChange(true);
-
-        }
-        TestCoordinator.check(TTChecks.QUESTION_SHOWN);
-
-    }
-
-    @Override
-    public HttpResponse requete() {
-        HttpResponse response = null;
-
-        try {
-            response = HttpComms.getInstance(this).getHttpResponse();
-        } catch (NetworkErrorException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return response;
-
-    }
-
+	public void setText(TextView view) {
+		text = view;
+	}
 }
