decb48155b127e715531e7c00e8cb4fda8c4c61b
Javier Rivas <javieralejandro.rivas@epfl.ch>
2013-10-30 23:14:50 +0100

    checkstyle

--- SwEng2013QuizApp/src/epfl/sweng/servercomm/CacheManager.java
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/CacheManager.java
@@ -3,28 +3,26 @@
  */
 package epfl.sweng.servercomm;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
-import epfl.sweng.authentication.CredentialManager;
 import android.content.Context;
 
 /**
  * @author Alex
- *
+ * 
  */
 public final class CacheManager {
-    private final static String CACHE_FILE = "cache.tmp";
-    private final static String OFFLINE_TASKS = "offline.tmp";
-    
+    /*
+     * private final static String CACHE_FILE = "cache.tmp";
+     * private final static String OFFLINE_TASKS = "offline.tmp";
+     */
+
     private static CacheManager sSingletonObject = null;
     private Context mContext;
-    private FileOutputStream mFileOut;
-    private FileInputStream mFileIn;
-    
-   
-  
-    
+
+    /*
+     * private FileOutputStream mFileOut;
+     * private FileInputStream mFileIn;
+     */
+
     public static CacheManager getInstance(Context cont) {
         if (sSingletonObject == null) {
             sSingletonObject = new CacheManager(cont);
@@ -32,21 +30,31 @@ public final class CacheManager {
         return sSingletonObject;
     }
 
-
     /**
      * @param context
      */
     public CacheManager(Context context) {
-             mContext = context;
-             
-    }
-    
-    
-    
-    
+        setContext(context);
 
-    
-    
-    
+    }
+
+    /**
+     * Return the the context
+     * 
+     * @return the context
+     */
+    public Context getContext() {
+        return mContext;
+    }
+
+    /**
+     * Set the context
+     * 
+     * @param context
+     *            the context to set
+     */
+    public void setContext(Context context) {
+        mContext = context;
+    }
 
 }
