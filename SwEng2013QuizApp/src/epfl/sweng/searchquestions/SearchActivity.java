f79ada6ece4e26b5347c6c8b115a8c7e2d8aa5a5
LorenzoLeon <lorenzo.leon@epfl.ch>
2013-11-15 15:29:55 +0100

    begining homework 5

---
+++ SwEng2013QuizApp/src/epfl/sweng/searchquestions/SearchActivity.java
@@ -0,0 +1,35 @@
+package epfl.sweng.searchquestions;
+
+import epfl.sweng.R;
+import epfl.sweng.testing.TestCoordinator;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
+import epfl.sweng.testing.TestingTransaction;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SearchActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is
+		// present.
+		getMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+
+	protected void onStart() {
+		TestCoordinator.check(TTChecks.);
+	}
+
+	public void search() {
+
+	}
+
+}
