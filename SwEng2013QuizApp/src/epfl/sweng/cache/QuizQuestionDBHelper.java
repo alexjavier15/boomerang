754269490e78569dc0634cfe19dad59f57246f1e
Noortje Verstegen <noortje.verstegen@epfl.ch>
2013-11-28 17:46:53 +0100

    bug database

--- SwEng2013QuizApp/src/epfl/sweng/cache/QuizQuestionDBHelper.java
+++ SwEng2013QuizApp/src/epfl/sweng/cache/QuizQuestionDBHelper.java
@@ -109,7 +109,7 @@ public class QuizQuestionDBHelper extends SQLiteOpenHelper implements BaseColumn
         SQLiteDatabase db = this.getWritableDatabase();
         Debug.out(getClass(), DatabaseUtils.queryNumEntries(db, TABLE_NAME));
 
-        db.delete(TABLE_NAME, _ID + "=?", new String[] {index});
+        db.delete(TABLE_NAME, _ID + "= ?", new String[] {index});
 
         Debug.out(getClass(), DatabaseUtils.queryNumEntries(db, TABLE_NAME));
         db.close();
@@ -123,7 +123,7 @@ public class QuizQuestionDBHelper extends SQLiteOpenHelper implements BaseColumn
         Cursor cursor = db.query(TABLE_NAME, new String[] {_ID, COLUMN_NAME_JSON_QUESTION}, null, null, null, null,
                 _ID + " ASC");
         if (cursor.moveToFirst()) {
-            cursorArray[0] = String.valueOf(cursor.getInt(CULUMN_JSON_INDEX));
+            cursorArray[0] = String.valueOf(cursor.getInt(0));
             cursorArray[1] = cursor.getString(CULUMN_JSON_INDEX);
 
         }
